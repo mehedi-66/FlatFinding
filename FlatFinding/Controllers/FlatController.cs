70f271dea75bcd5aa5a270da9b478fb8bd43e6fb
Mehedi Hasan <mehedisn3@gmail.com>
2023-07-17 02:07:56 +0600

    Flat list and Flat Details part desing

---
+++ FlatFinding/Controllers/FlatController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlatFinding.Controllers
+{
+    [Authorize]
+    public class FlatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FlatDetails()
+        {
+            return View();
+        }
+    }
+}
