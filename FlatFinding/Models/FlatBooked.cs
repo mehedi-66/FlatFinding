2b2ca594373e594ae4550625a8255fd50ca844d1
Mehedi Hasan <19303001@iubat.edu>
2023-09-01 11:46:47 +0600

    model of police verification

--- FlatFinding/Models/FlatBooked.cs
+++ FlatFinding/Models/FlatBooked.cs
@@ -21,6 +21,26 @@ namespace FlatFinding.Models
         public int IsDelete { get; set; }
         public DateTime? BookingCancel { get; set; }
 
+        // Police varification 
+        public string?  PictureOfPerson {get; set;}
+        public string? NameOfWhoLive { get; set; }
+        public string? FatherName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? ParmanetAddress { get; set; }
+        public string? MaritalStatus { get; set; }
+        public string? EmploymentAddress { get; set; }
+        public string? Religion { get; set; }
+        public string? PhoneNumberOfWhoTakeFlat { get; set; }
+        public string? EmailId { get; set; }
+        public string? NID { get; set; }
+        public string? Person1Name { get; set; }
+        public string? Person1Phone { get; set; }
+        public string? Person2Name { get; set; }
+        public string? Person2Phone { get; set; }
+        public string? NameOfBeforeHouseHolderName { get; set; }
+        public string? AddressOfBeforeHouse { get; set; }
+        public string? ResoneOfLeveingHouse { get; set; }
 
+        public int? Condition { get; set; }
     }
 }
